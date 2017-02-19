d620c1fcfb51afdf7d86e866c85dd9f936f6127b
mizuki2232 <mizuki.kamata@serverworks.co.jp>
2017-02-19 12:57:34 +0900

    Update convert_stl.py

--- www/convert_stl.py
+++ www/convert_stl.py
@@ -9,11 +9,10 @@ from scipy.ndimage import gaussian_filter
 from pylab import imread
 
 client = boto3.client('sqs')
-QueueUrl = 'https://sqs.ap-northeast-1.amazonaws.com/743424264703/Image'
 
 while 1:
     que_message = client.receive_message(
-        QueueUrl = 'https://sqs.ap-northeast-1.amazonaws.com/743424264703/Image'
+        QueueUrl = 'yourQueueUrl'
 )
     if "Messages" in que_message:
         records = que_message['Messages'][0]['Body']
@@ -29,14 +28,14 @@ while 1:
             print "wrong queue"
             break;
         s3 = boto3.resource('s3')
-        s3.Object('smart-3d', key).download_file(key)
+        s3.Object('your-bucketname', key).download_file(key)
         A = 256 * imread(key)
         A = A[:, :, 2] + 1.0*A[:,:, 0] # Compose RGBA channels to give depth
         A = gaussian_filter(A, 1)  # smoothing
         numpy2stl(A, 'examples.stl', scale=0.05, mask_val=5., solid=True)
-        s3.meta.client.upload_file('examples.stl', 'smart-3d', 'examples.stl')
+        s3.meta.client.upload_file('examples.stl', 'yourbucket-name', 'examples.stl')
         client.delete_message(
-            QueueUrl = 'https://sqs.ap-northeast-1.amazonaws.com/743424264703/Image',
+            QueueUrl = 'your-QueurUrl',
             ReceiptHandle = que_message["Messages"][0]["ReceiptHandle"]
                 )
         print "successfully uploaded!"
